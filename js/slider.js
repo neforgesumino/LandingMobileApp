77e7fddfa5563bd4453df6d2bf15ba981ad08f91
neforgesumino <57845270+neforgesumino@users.noreply.github.com>
2020-04-01 22:53:06 +0300

    completely errored + jquery cancelled
    + will have to replace jquery, don't like it performance.

--- js/slider.js
+++ js/slider.js
@@ -6,27 +6,3 @@ $(document).ready(function() {
     // slides counter 
     var counter = 0;
 
-    function slider_func() {
-        // Repeat the loop after every given time interval
-        setTimeout(slider_func, 3000);
-        // hide all slides
-        var j = 0;
-        while (j < total_slides) {
-          $(slides[j]).css("display", "none");
-          j++;
-        }
-
-        // show a slide and animate
-        $(slides[counter])
-        .css("display", "block")
-        .css("animation", "popin 2s");
-        counter++;
-
-
-        // reset counter
-        if (counter >= total_slides) {
-          counter = 0;
-         }
-    }
-        slider_func();
-});

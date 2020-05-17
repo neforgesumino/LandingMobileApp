e54330962597cde39127464d6e8c301a992ea744
neforgesumino <57845270+neforgesumino@users.noreply.github.com>
2020-05-17 23:04:21 +0300

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,4 @@
 $(document).ready(function() {
     // slides array
-    const slides = document.getElementsByClassName("slides");
-    // total no. slides
-    const total_slides = slides.length;
-    // slides counter 
-    var counter = 0;
+ 
 
